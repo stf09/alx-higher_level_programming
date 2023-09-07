b5f3c4e678041722ae57f2724b6e330c595bfe9c
stf09 <bikemlit32@gmail.com>
2023-09-07 12:29:12 +0000

    5-variable_load.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,9 +4,9 @@ import hidden_4
 
 def discovr():
     name = dir(hidden_4)
-    for i in name:
-        if i[:2] != '__':
-            print("{:s}".format(i))
+    for k in name:
+        if k[:2] != '__':
+            print("{:s}".format(k))
 
 
 if __name__ == "__main__":
