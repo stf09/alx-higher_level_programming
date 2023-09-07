6031ec99c6a96d252b98601eae2a8dd744a4103a
stf09 <bikemlit32@gmail.com>
2023-09-07 12:39:28 +0000

    100-my_calculator.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
+def print_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d} argument.".format(n))
+        return
+    else:
+        if n == 1:
+            print("{:d} argument:".format(n))
+        else:
+            print("{:d} arguments:".format(n))
+        i = 1
+        while i <= n:
+            print("{:d}: {:s}".format(i, argv[i]))
+            i += 1
+
 if __name__ == "__main__":
     import sys
-    args = len(sys.argv) - 1
-    if args is 0:
-        print("{:d} arguments.".format(args))
-    elif args is 1:
-        print("{:d} argument:".format(args))
-    else:
-        print("{:d} arguments:".format(args))
-    for i in range(1, len(sys.argv)):
-        print("{:d}: {:s}".format(i, sys.argv[i]))
-
+    print_arg(sys.argv)
