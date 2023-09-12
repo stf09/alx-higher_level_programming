60f52793cf4e3f44befc9025b440193d20050018
stf09 <bikemlit32@gmail.com>
2023-09-12 04:14:36 +0000

    11-delete_at.py

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx < len(my_list):
+       return (my_list)
+    else:
+       for a in my_list:
+           if a == idx:
+               my_list[a] = ""
+    return (my_list)
+
