7d045d08c7a6066ad9876515872a566909b65096
stf09 <bikemlit32@gmail.com>
2023-09-12 05:39:49 +0000

    100-print_python_list_info.c

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    c = my_list[0]
-    for x in my_list:
-        if x > c:
-            c = x
-    return(c)
+    if len(my_list) == 0:
+        return (None)
+
+    big = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > big:
+            big = my_list[i]
+
+    return (big)
