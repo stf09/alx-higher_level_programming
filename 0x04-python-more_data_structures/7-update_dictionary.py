2ae2cc9e7e8e9ad49a841bcb5234967be40918a9
stf09 <bikemlit32@gmail.com>
2023-09-13 18:34:59 +0000

    6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-     a_dictionary[key] = value
-     return (a_dictionary) 
+    """Replace or add key/value pairs in a dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
