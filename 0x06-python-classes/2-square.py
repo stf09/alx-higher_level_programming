48438f962941c6ff9cb9b9ab3df97e3d88950dbe
stf09 <bikemlit32@gmail.com>
2023-09-26 14:03:45 +0000

    1-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,6 +3,7 @@
 
 
 class Square:
+    """ Square class"""
     def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ creates class Square """
 
+
 class Square:
+    """ Square class"""
     def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
