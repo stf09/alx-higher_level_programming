de995c17fc25052843886958c9ae7982a92cb6b9
stf09 <bikemlit32@gmail.com>
2024-01-07 00:33:27 -0800

    1-hbtn_header.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,5 +8,5 @@ from sys import argv
 
 if __name__ == "__main__":
     data = parse.urlencode({"email": argv[2]}).encode('utf-8')
-    with request.urlopen(argv[2], data) as page:
+    with request.urlopen(argv[1], data) as page:
         print(page.read().decode('utf-8'))
